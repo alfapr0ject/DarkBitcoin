a44f72f6e54fdece40f70d0e33332d1c8bf92dc1
Chris Moore <dooglus@gmail.com>
2014-11-22 20:41:37 -0800

    Fix to work with unconfirmed outputs and unconfirmed spends. Also show how an output is spent, if it is.

--- src/main.cpp
+++ src/main.cpp
@@ -902,7 +902,7 @@ int CTxIndex::GetDepthInMainChain() const
 }
  
 // Return transaction in tx, and if it was found inside a block, its hash is placed in hashBlock
-bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock)
+bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock, CTxIndex& txindex)
 {
     {
         LOCK(cs_main);
@@ -913,7 +913,6 @@ bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock)
             }
         }
         CTxDB txdb("r");
-        CTxIndex txindex;
         if (tx.ReadFromDisk(txdb, COutPoint(hash, 0), txindex))
         {
             CBlock block;
@@ -923,6 +922,12 @@ bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock)
         }
     }
     return false;
+}
+
+bool GetTransaction(const uint256 &hash, CTransaction &tx, uint256 &hashBlock)
+{
+    CTxIndex txindex;
+    return GetTransaction(hash, tx, hashBlock, txindex);
 }
  
  
